f7561660b4bd871ddee15a6b58ff9ed3b67c593d
Aleksandr Bordiugov <a@bordiugov.ru>
2025-02-15 07:31:48 +0300

    Remove useles import. Add human readable names for input fields.

--- term/2/python/less3/main.py
+++ term/2/python/less3/main.py
@@ -1,4 +1,5 @@
 import os
+from math import ceil
 
 def cls():
   os.system('cls' if os.name == 'nt' else 'clear')
@@ -20,14 +21,17 @@ match choosen:
     
     inputRequest = {
       "age": {
+          "name": "Возраст",
           "text": "Введите возраст вашего питомца",
           "expectedValType": "int"
         },
       "name": {
+        "name": "Имя",
         "text": "Введите имя вашего питомца",
         "expectedValType": "str"
       },
       "petType": {
+        "name": "Тип питомца",
         "text": "Введите тип вашего питомца",
         "expectedValType": "str"
       }
@@ -50,13 +54,14 @@ match choosen:
       return "лет"
     # end def
     
-    def checkData(data, isFirstInit):
+    def checkData(data, inputRequest, isFirstInit):
       isFilled = True
 
       for k, v in data.items():
         if not v:
           isFilled = False
-          print(f"Поле \"{k}\" заполнено некорректно. Попробуйте еще раз.")
+          print(f"Поле \"{inputRequest[k]['name']}\" заполнено некорректно. Попробуйте еще раз.")
+      # end for
 
       if not isFirstInit and not isFilled: os.system('pause')
       
@@ -80,7 +85,7 @@ match choosen:
     def handler(petInfo, inputRequest):
       global isFirstInit
       
-      if not checkData(petInfo, isFirstInit):
+      if not checkData(petInfo, inputRequest, isFirstInit):
         isFirstInit = False
         filler(petInfo, inputRequest)
         handler(petInfo, inputRequest)
@@ -92,7 +97,102 @@ match choosen:
     
 #task №2
   case "2":
-    pass
+    tests = [
+      {
+        "question": "Назовите первый этап эволюции человека:",
+        "answers": ['дриопитек'],
+        "userAnswer": ""
+      },
+      {
+        "question": "Назовите второй этап эволюции человека:",
+        "answers": ['стадия протантропа', 'протантроп', 'австралопитек'],
+        "userAnswer": ""
+      },
+      {
+        "question": "Назовите третий этап эволюции человека:",
+        "answers": ['человек умелый', 'умелый'],
+        "userAnswer": ""
+      },
+      {
+        "question": "Назовите четвертый этап эволюции человека:",
+        "answers": ['стадия архантропа', 'архантроп', 'стадия питекантропа', 'питекантроп', 'человек прямоходящий', 'прямоходящий'],
+        "userAnswer": ""
+      },
+      {
+        "question": "Назовите пятый этап эволюции человека:",
+        "answers": ['стадия палеоантропа', 'палеоантроп', 'стадия неандертальца', 'неандерталец'],
+        "userAnswer": ""
+      },
+      {
+        "question": "Назовите шестой этап эволюции человека:",
+        "answers": ['стадия неоантропа', 'неоантроп', 'кроманьонец', 'стадия человека разумного', 'человек разумный', 'разумный'],
+        "userAnswer": ""
+      }
+    ]
+    
+    def isAnswerCorrect(answersList, userAnswer):
+      if not len(userAnswer): return False
+      
+      percentToCorrect = 70
+      letterToPercent = ceil(100 / len(userAnswer))
+      maxPercentOfCorrect = 0
+      
+      for answer in answersList:
+        answerLen = len(answer)
+        correctLettersPercent = 0
+        
+        for i in range(0, len(userAnswer)):
+          UAC = userAnswer[i]
+          AC = answer[i] if i < answerLen else None
+          
+          if UAC == AC: correctLettersPercent += letterToPercent
+        # end for
+        
+        if correctLettersPercent > maxPercentOfCorrect:
+          maxPercentOfCorrect = correctLettersPercent
+      # end for
+
+      return maxPercentOfCorrect >= percentToCorrect
+    # end def
+    
+    def startTest(tests):
+      for test in tests:
+        verdict = ""
+        
+        cls()
+        print(test['question'])
+        test['userAnswer'] = input()
+        
+        if not isAnswerCorrect(test['answers'], test['userAnswer']):
+          verdict = "неверно"
+        else:
+          verdict = "верно"
+          
+        test['userAnswer'] = f"\"{test['userAnswer']}\" ({verdict})"
+      # end for
+      
+      result(tests)
+    # end def
+    
+    def result(tests):
+      testLen = len(tests)
+      concStr = " => "
+      alternatePrint = []
+      
+      cls()
+      print("Ваши варианты ответов:")
+      
+      for i in range(0, testLen):
+        endStr = concStr if i + 1 < testLen else ""
+        str = f"{i + 1}. {tests[i]['userAnswer']}"
+        alternatePrint.append(str)
+        print(str, end=endStr)
+        
+      print("\n-----------------------------------------")
+      print(*alternatePrint, sep=concStr)
+    # end def
+    
+    startTest(tests)
 
   case _:
     _print("Получено некорректное значение. Запустите программу повторно и введите цифру от 1 до 2.")
